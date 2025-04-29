158bb74bc6d182f1f3791ce20bf04e7ef40f57b4
ppablolds <pablosilvaaluno@outlook.com>
2025-04-29 15:29:19 -0300

    feat: Create a interface AtendimentoRepository

--- src/main/java/dev/atendimentoAPI/atendimento/repository/AtendimentoRepository.java
+++ src/main/java/dev/atendimentoAPI/atendimento/repository/AtendimentoRepository.java
@@ -2,5 +2,7 @@ package dev.atendimentoAPI.atendimento.repository;
 
 import dev.atendimentoAPI.atendimento.model.Atendimento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AtendimentoRepository extends JpaRepository<Atendimento, Long> {}
