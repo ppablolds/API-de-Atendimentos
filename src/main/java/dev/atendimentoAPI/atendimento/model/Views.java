4a647a98897c07d097bff1854dc75210cd8abbf9
ppablolds <pablosilvaaluno@outlook.com>
2025-05-19 13:59:44 -0300

    feat: Add static methods for JsonView

--- src/main/java/dev/atendimentoAPI/atendimento/model/Views.java
+++ src/main/java/dev/atendimentoAPI/atendimento/model/Views.java
@@ -3,4 +3,6 @@ package dev.atendimentoAPI.atendimento.model;
 public class Views {
     public static class Registro {}
     public static class Login {}
+    public static class CriarAtendimento {}
+    public static class BuscarAtendimento {}
 }
