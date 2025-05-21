78e46739ed3fcff9b32caa2fe8c62950f5bfc50b
ppablolds <pablosilvaaluno@outlook.com>
2025-05-21 17:41:15 -0300

    feat: Update ApiExceptionHandler and pom.xml

--- src/main/java/dev/atendimentoAPI/atendimento/exception/ApiExceptionHandler.java
+++ src/main/java/dev/atendimentoAPI/atendimento/exception/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package dev.atendimentoAPI.atendimento.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import java.util.HashMap;
 import java.util.Map;
 
+@Hidden
 @ControllerAdvice
 public class ApiExceptionHandler {
 
